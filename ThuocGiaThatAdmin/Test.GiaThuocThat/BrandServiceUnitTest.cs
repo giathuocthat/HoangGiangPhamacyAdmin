9058641ed5c7176c9ac020198aebdf7906e1f4b0
Khanh Nguyen <khanh120@gmail.com>
2025-12-01 08:54:00 +0700

    update

---
+++ ThuocGiaThatAdmin/Test.GiaThuocThat/BrandServiceUnitTest.cs
@@ -0,0 +1,82 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThuocGiaThat.Infrastucture.Repositories;
+using ThuocGiaThatAdmin.Domain.Entities;
+using ThuocGiaThatAdmin.Service.Interfaces;
+using ThuocGiaThatAdmin.Service.Services;
+
+namespace Test.GiaThuocThat
+{
+    [TestFixture]
+    public class BrandServiceUnitTest
+    {
+        private  IBrandService _brandService;
+        private  Mock<IBrandRepository> _mockBrandRepository;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _mockBrandRepository = new Mock<IBrandRepository>(MockBehavior.Strict);
+            _brandService = new BrandService(_mockBrandRepository.Object);
+        }
+
+        [Test]
+        public void GetBrand_ShouldReturnBrand_WhenBrandExists()
+        {
+            // Arrange
+            var brandId = 1;
+            var expectedBrand = new Brand
+            {
+                Id = brandId,
+                Name = "Test Brand",
+                Slug = "test-brand"
+            };
+
+            _mockBrandRepository
+                .Setup(repo => repo.GetByIdAsync(brandId))
+                .ReturnsAsync(expectedBrand);
+            // Act
+            var result = _brandService.GetBrandByIdAsync(brandId).Result;
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedBrand.Id, result.Id);
+            Assert.AreEqual(expectedBrand.Name, result.Name);
+            Assert.AreEqual(expectedBrand.Slug, result.Slug);
+            _mockBrandRepository.Verify(repo => repo.GetByIdAsync(brandId), Times.Once);
+        }
+
+        [Test]
+        public void GetBrand_ShouldReturnNull_WhenBrandDoesNotExist()
+        {
+            // Arrange
+            var brandId = 999; // Non-existing brand ID
+            _mockBrandRepository
+                .Setup(repo => repo.GetByIdAsync(brandId))
+                .ReturnsAsync((Brand?)null);
+            // Act
+            var result = _brandService.GetBrandByIdAsync(brandId).Result;
+            // Assert
+            Assert.IsNull(result);
+            _mockBrandRepository.Verify(repo => repo.GetByIdAsync(brandId), Times.Once);
+        }
+
+        [Test]
+        public void GetBrand_ShouldThrowArgumentException_WhenBrandIdIsInvalid()
+        {
+            // Arrange
+            var invalidBrandId = -1;
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ArgumentException>(async () =>
+                await _brandService.GetBrandByIdAsync(invalidBrandId));
+
+            Assert.That(ex.Message, Does.Contain("Brand ID must be greater than 0"));
+
+            _mockBrandRepository.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Never);
+        }
+    }
+}
---
+++ ThuocGiaThatAdmin/ThuocGiaThatAdmin.Service/Interfaces/IBrandService.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ThuocGiaThatAdmin.Domain.Entities;
+
+namespace ThuocGiaThatAdmin.Service.Interfaces
+{
+    /// <summary>
+    /// Service interface for Brand business logic
+    /// Defines contract for brand management operations
+    /// </summary>
+    public interface IBrandService
+    {
+        #region Read Operations
+
+        /// <summary>
+        /// Get brands with pagination
+        /// </summary>
+        /// <param name="pageNumber">Page number (1-based), defaults to 1</param>
+        /// <param name="pageSize">Number of items per page (1-100), defaults to 10</param>
+        /// <returns>Tuple containing list of brands and total count</returns>
+        /// <exception cref="ArgumentException">Thrown when pageNumber is less than or equal to 0, or pageSize is not between 1 and 100</exception>
+        Task<(IEnumerable<Brand> Brands, int TotalCount)> GetBrandsAsync(int pageNumber = 1, int pageSize = 10);
+
+        /// <summary>
+        /// Get brand by ID
+        /// </summary>
+        /// <param name="id">Brand ID</param>
+        /// <returns>Brand if found, null otherwise</returns>
+        /// <exception cref="ArgumentException">Thrown when ID is less than or equal to 0</exception>
+        Task<Brand?> GetBrandByIdAsync(int id);
+
+        #endregion
+
+        #region Update Operation
+
+        /// <summary>
+        /// Update an existing brand
+        /// </summary>
+        /// <param name="brand">Brand entity with updated values</param>
+        /// <returns>Number of entities affected in the database</returns>
+        /// <exception cref="ArgumentNullException">Thrown when brand is null</exception>
+        /// <exception cref="ArgumentException">Thrown when brand ID is less than or equal to 0, or name is null or empty</exception>
+        /// <exception cref="InvalidOperationException">Thrown when brand with specified ID is not found</exception>
+        Task<int> UpdateBrandAsync(Brand brand);
+
+        #endregion
+
+        #region Delete Operation
+
+        /// <summary>
+        /// Delete a brand by ID
+        /// </summary>
+        /// <param name="id">Brand ID to delete</param>
+        /// <returns>Number of entities affected in the database</returns>
+        /// <exception cref="ArgumentException">Thrown when ID is less than or equal to 0</exception>
+        /// <exception cref="InvalidOperationException">Thrown when brand is not found or brand is being used by products</exception>
+        Task<int> DeleteBrandAsync(int id);
+
+        #endregion
+    }
+}
