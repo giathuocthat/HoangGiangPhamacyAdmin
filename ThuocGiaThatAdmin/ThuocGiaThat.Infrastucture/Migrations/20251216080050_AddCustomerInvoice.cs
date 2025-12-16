1e8a7d8e05858025567156a4caf30f58d0706590
haibt81-git <haibt81@gmail.com>
2025-12-16 15:01:13 +0700

    Thêm bảng customer invoice

---
+++ ThuocGiaThatAdmin/ThuocGiaThat.Infrastucture/Migrations/20251216080050_AddCustomerInvoice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThuocGiaThat.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerInvoiceInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    BuyerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TaxCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InvoiceAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerInvoiceInfo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerInvoiceInfo_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerInvoiceInfo_CustomerId",
+                table: "CustomerInvoiceInfo",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerInvoiceInfo");
+        }
+    }
+}
